d86f5d4520fc153f04bd443bd89b0819ec9765f7
Mykyta Mykulskyi <81817136+Mik-813@users.noreply.github.com>
2025-11-07 18:53:23 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Web development dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            php84
+            php84Packages.composer
+            laravel
+            nodejs
+            nginx
+            certbot
+          ];
+ 
+          shellHook = ''
+            [ -n "$FLAKE_ENV" ] && exit
+            export FLAKE_ENV=1
+
+            PS1='\[\033[35m\][\[\033[34m\]nix-flake\[\033[35m\]]\[\033[0m\] \w \$ '
+            export PATH="$PWD/vendor/bin:$PATH"
+          '';
+        };
+      }
+    );
+}
